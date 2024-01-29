ba629a6d32158e0dc892296ce27709782f6e16e4
khushal <singhkhushal129@gmail.com>
2024-01-29 22:57:42 +0530

    First Page Anim & Cursor Change

--- script.js
+++ script.js
@@ -3,3 +3,36 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
+function firstPageAnim(){
+    var tl = gsap.timeline();
+    tl.from("#nav", {
+        y:'-10',
+        opacity:0,
+        duration: 1.5,
+        ease: Expo.easeInOut
+    })
+    .to(".boundingel",{
+        y:0,
+        ease: Expo.easeInOut,
+        duration: 1.5,
+        delay: -1,
+        stagger: .2
+    })
+    .from("#herofooter", {
+        y:'-10',
+        opacity:0,
+        duration: 1.5,
+        delay: -1,
+        ease: Expo.easeInOut
+    })
+}
+function mouseFollower(dets){
+window.addEventListener("mousemove",function(dets){
+document.querySelector("#minicircle").style.transform =`translate(${dets.clientX}px, ${dets.clientY}px)`;
+})
+   
+}
+
+mouseFollower(); 
+firstPageAnim();
+
